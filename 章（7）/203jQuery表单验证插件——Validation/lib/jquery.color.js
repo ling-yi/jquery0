cb1ec2166f2d3da2052b6194832d6ed1d8770170
ling-yi <1695877968@qq.com>
2018-11-20 21:46:20 +0800

    提交整个jQuery学习过程例子

---
+++ 章（7）/203jQuery表单验证插件——Validation/lib/jquery.color.js
@@ -0,0 +1,11 @@
+;(function($){
+	jQuery.fn.extend({
+		"color":function(value){
+			if (value == undefined) {
+				return this.css("color");
+			} else{
+				return this.css("color",value);
+			}
+		}
+	});
+})(jQuery);
